dde1c8064edb8b9c36e5f2d6138bc7fcafc7b4ff
Mikael Larsson <micke.lar@gmail.com>
2022-03-09 08:59:15 +0100

    Add project files.

---
+++ Gymbokning/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Gymbokning.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gymbokning.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUserGymClass>()
+                .HasKey(t => new {t.ApplicationUserId, t.GymClassId});
+        }
+
+        public DbSet<GymClass> GymClasses { get; set; }
+    }
+}
---
+++ Gymbokning/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Gymbokning.Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<ApplicationUserGymClass> AttendedClasses { get; set; }
+    }
+}
